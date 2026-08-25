fbd2f89e4b1149aa776d52e0f3921125dbe76069
aivastudioindo <aivastudioindo@users.noreply.github.com>
2026-08-25 20:30:43 +0700

    Naikkan Kotlin ke 2.2.20 (minimum Flutter 3.47)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     // START: FlutterFire Configuration
     // END: FlutterFire Configuration
     id("dev.flutter.flutter-gradle-plugin") apply false
